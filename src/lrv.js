381c07e1643e0e4286d53e3a4a7b81c386e72308
Christos Dukakis <chrisdukakis@gmail.com>
2024-03-04 23:16:54 +0200

    await the cursor value

--- src/lrv.js
+++ src/lrv.js
@@ -1248,7 +1248,7 @@ export const lrv = function (numberOfStoredTicks = MAX_NUMBER_OF_TICKS_PER_EPOCH
 
                                 async getTransactions(offset, count) {
                                     // TODO: sign keys of the store and verify signature before accepting this data
-                                    return (await Promise.all(await (await store).getRange(offset, count))).map(async ({ state, value }) => Object.freeze({
+                                    return (await (await store).getRange(offset, count)).map(async ({ state, value }) => Object.freeze({
                                         ...(await inspectTransaction(value)),
                                         processed: state,
                                     }).filter((transaction) => transaction.sourceId === id));
--- src/utils.js
+++ src/utils.js
@@ -156,7 +156,7 @@ const idb = function (name) {
                 const data = [];
                 let i = 0;
 
-                request.onsuccess = (event) => {
+                request.onsuccess = async (event) => {
                     const cursor = event.target.result;
                     if (i === 0 && offset > 0) {
                         cursor.advance(offset);
@@ -165,7 +165,7 @@ const idb = function (name) {
                         if (cursor.key.indexOf('-')) {
                             data.push({
                                 state: cursor.key.indexOf('-failed') === -1 ? true : false,
-                                value: cursor.value,
+                                value: await cursor.value,
                             });
 
                             if (++i < count) {
